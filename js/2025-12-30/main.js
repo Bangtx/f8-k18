48e207a87689a8cbac473e3ef8113736fc18613c
Bangtx <banhtran.hha@gmail.com>
2025-12-30 22:01:50 +0700

    operator

---
+++ js/2025-12-30/main.js
@@ -0,0 +1,84 @@
+// const numbers = [4, 3, 1, 5, 1, 1000]
+// // numbers = 0x01
+//
+// const copiedNumbers = numbers
+// // copied_numbers = 0x01
+//
+// copiedNumbers[1] = 2000
+//
+//
+// console.log(numbers)
+//
+// const a = 1
+// let b = a
+//
+// b = 2
+// console.log(a)
+
+
+// const a = [1, 2, 3]
+// let b = a
+// b[0] = 10
+// console.log(a)
+
+// const studentA = {
+//   name: 'A',
+//   age: 23,
+//   scores: [0, 1, 3]
+// }
+
+// 0x01
+
+// console.log(studentA.name, studentA.age)
+// console.log(studentA.scores[1])
+
+
+// const scores = studentA.scores
+// scores[0] = 10
+// console.log(studentA.scores)
+//
+// const studentB = studentA
+// // 0x01
+// studentB.name = 'B'
+//
+// console.log(studentA)
+//
+
+
+// let a = "100"
+// let b = "2"
+// let c = a - b
+// // "1" + "1" -> "11"
+//
+// // NaN
+// console.log(c)
+
+
+const a = false
+const b = true
+
+console.log(!a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
