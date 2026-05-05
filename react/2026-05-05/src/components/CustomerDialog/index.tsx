8c2626423a9b83e61b02ef3a06f4993f6ffcb3be
Bangtx <banhtran.hha@gmail.com>
2026-05-05 21:23:28 +0700

    new project

--- react/2026-05-05/src/components/CustomerDialog/index.tsx
+++ react/2026-05-05/src/components/CustomerDialog/index.tsx
@@ -1,44 +1,22 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
+import DialogContainer from "../DialogContainer";
+import {TextField} from "@mui/material";
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-  // @ts-ignore
-  return <Slide direction="up" ref={ref} {...props} />;
-});;
-
-const CustomerDialog = ({isOpen, handleClose}) => {
+const CustomerDialog = ({isOpen, onClose}) => {
   return (
     <>
-      <Dialog
-        open={isOpen}
-        slots={{
-          transition: Transition,
-        }}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-        role="alertdialog"
+      <DialogContainer
+        isOpen={isOpen}
+        onClose={onClose}
       >
-        <DialogTitle>{"Customer Dialog"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} autoFocus color={'error'}>
-            Cancel
-          </Button>
-          <Button onClick={handleClose}>Agree</Button>
-        </DialogActions>
-      </Dialog>
+        <TextField
+          fullWidth
+          label={'name'}
+        />
+        <TextField
+          fullWidth
+          label={'address'}
+        />
+      </DialogContainer>
     </>
   )
 }
